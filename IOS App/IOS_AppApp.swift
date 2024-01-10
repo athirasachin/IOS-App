f7b8526e0b14153bca0f5a0daf60502ea032aa88
Athira Mangalath Muraleedharan <Amangalathmural5484@conestogac.on.ca>
2024-01-10 18:17:09 -0500

    Initial Commit

---
+++ IOS App/IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_AppApp.swift
+//  IOS App
+//
+//  Created by user235514 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
